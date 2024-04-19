4e3fa056a6f8289d94d7d169d17a35e516872c5d
lealkedna <capic.2021218minf0175@aluno.ifpi.edu.br>
2024-04-19 12:55:37 +0100

    mudando o design

---
+++ js/script.js
@@ -0,0 +1,337 @@
+const menu = [
+    {
+        id: 1,
+        title: "Coxinha",
+        categoria: "Salgados",
+        preco: 4,
+        img: "img/coxinha.jpeg",
+        descricao: "Coxinha de frango",
+        btn:" Fazer pedido",
+    },
+
+    {
+        id: 2,
+        title: "Suco",
+        categoria: "Bebidas",
+        preco: 3,
+        img: "img/bebidas-sucos.avif",
+        descricao: "Suco de Laranja",
+        btn: "Fazer pedido",
+    },
+
+    {
+        id: 3,
+        title: "Brigadeiro",
+        categoria: "Sobremesas",
+        preco: 3.50,
+        img: "img/sobremesa-bricadeiro.jpeg",
+        descricao: "Sabor chocolate",
+        btn: "Fazer pedido",
+    },
+    
+
+    {
+        id: 4,
+        title: "Pastel",
+        categoria: "Salgados",
+        preco: 4,
+        img: "img/pastel-carne.jpeg",
+        descricao: "Pastel de carne",
+        btn:" Fazer pedido",
+    },
+
+    {
+        id: 5,
+        title: "Feijoada",
+        categoria: "tipicas",
+        preco: 15,
+        img: "img/feijoada.jpeg",
+        descricao: "Porção de Feijoada</br> serve 3 pessoas",
+        btn:" Fazer pedido",
+    },
+
+    
+    {
+        id: 6,
+        title: "Pizza de Calabresa",
+        categoria: "pizza",
+        preco: 40,
+        img: "img/pizza-calabresa.jpeg",
+        descricao: "tamanho: Média</br>queijo mussarela</br>azeitona</br>porção para 4 pessoas",
+        btn:" Fazer pedido",
+    },
+
+
+    {
+        id: 7,
+        title: "Hambúguer",
+        categoria: "hamburguer",
+        preco: 15,
+        img: "img/hamburguer-caseiro.jpg",
+        descricao: "Pão Artersanal</br>2carnes</br>2queijos</br>2bacon",
+        btn:" Fazer pedido",
+    },
+
+
+    {
+        id: 8,
+        title: "Pastel",
+        categoria: "Salgados",
+        preco: 4,
+        img: "img/pastel-misto.jpg",
+        descricao: "Pastel misto ",
+        btn:" Fazer pedido",
+    },
+ 
+
+    {
+        id: 9,
+        title: "Coxinha",
+        categoria: "Salgados",
+        preco: 4,
+        img: "img/coxinha-carne.jpeg",
+        descricao: "Coxinha de carne",
+        btn:" Fazer pedido",
+    },
+
+    {
+        id: 11,
+        title: "Refri",
+        categoria: "Bebidas",
+        preco: 6.50,
+        img: "img/bebida-refri-laranja.jpeg",
+        descricao: "Fanta de Laranja",
+        btn:" Fazer pedido",
+    },
+
+    {
+        id: 12,
+        title: "Refri",
+        categoria: "Bebidas",
+        preco: 10,
+        img: "img/bebida-refi-coca.jpeg",
+        descricao: "Coca-Cola",
+        btn:" Fazer pedido",
+    },
+
+    {
+        id: 13,
+        title: "Bolo",
+        categoria: "Sobremesas",
+        preco: 3,
+        img: "img/sobremesa-bolo.jpg",
+        descricao: "Bolo de chocolate com</br> sorvete de leite condensado</br>cobertura  de chocolate",
+        btn:" Fazer pedido",
+    },
+
+
+    {
+        id: 14,
+        title: "Pastel",
+        categoria: "Salgados",
+        preco: 4,
+        img: "img/pasteal-frango.jpg",
+        descricao: "Pastel de frango",
+        btn:" Fazer pedido",
+    },
+
+
+    {
+        id: 15,
+        title: "Sorvete",
+        categoria: "Sobremesas",
+        preco: 3,
+        img: "img/sobremesa-choco.jpg",
+        descricao: "Brownie na taça",
+        btn:" Fazer pedido",
+    },
+
+    {
+        id: 16,
+        title: "Cuscuz",
+        categoria: "tipicas",
+        preco: 10,
+        img: "img/cuscuzreceado.jpg",
+        descricao: "Recheado com queijo",
+        btn:" Fazer pedido",
+    },
+
+    {
+        id: 17,
+        title: "Maria Isabel",
+        categoria: "tipicas",
+        preco: 15,
+        img: "img/mariaIsabel.jpeg",
+        descricao: "Recheado com carne de sol</br>porção para 2 pessoas",
+        btn:" Fazer pedido",
+    },
+
+    {
+        id: 18,
+        title: "Baião de dois",
+        categoria: "tipicas",
+        preco: 20,
+        img: "img/baiao.jpg",
+        descricao: "Arroz com feijão</br>2Queijos</br>porção para 2 pessoas",
+        btn:" Fazer pedido",
+    },
+
+    {
+        id: 19,
+        title: "Bolinho de Macaxeira",
+        categoria: "tipicas",
+        preco: 5,
+        img: "img/bolinho-de-macaxeira.jpeg",
+        descricao: "Recheado com queijo</br>porção para 1 pessoas",
+        btn:" Fazer pedido",
+    },
+
+    {
+        id: 20,
+        title: "Carne-de-Sol",
+        categoria: "tipicas",
+        preco: 25,
+        img: "img/carne-de-sol.jpg",
+        descricao: "carne de sol acebolada</br>macaxeira frita</br>porção para 2 pessoas",
+        btn:" Fazer pedido",
+    },
+
+    {
+        id: 21,
+        title: "Vinagrete",
+        categoria: "tipicas",
+        preco: 3,
+        img: "img/vinagrete.jpeg",
+        descricao: "porção para 2 pessoas",
+        btn:" Fazer pedido",
+    },
+
+
+    {
+        id: 22,
+        title: "Pizza de Carne Seca",
+        categoria: "pizza",
+        preco: 40,
+        img: "img/pizza-carne.jpg",
+        descricao: "tamanho: Médio</br>sem borda</br>queijo mussarela</br>porção para 4 pessoas",
+        btn:" Fazer pedido",
+    },
+
+    {
+        id: 23,
+        title: "Pizza de Papperoni",
+        categoria: "pizza",
+        preco: 40,
+        img: "img/pizza-de-pepperoni.jpg",
+        descricao: "tamanho: Médio</br>com borda de catupiry</br>azeitonas</br>milho</br>pimentões</br>porção para 4 pessoas",
+        btn:" Fazer pedido",
+    },
+
+    {
+        id: 24,
+        title: "Pizzas",
+        categoria: "pizza",
+        preco: 60,
+        img: "img/pizza-duas.jpeg",
+        descricao: "tamanho: pequeno</br>calabresa</br>carne seca</br>milho</br>porção para 6 pessoas",
+        btn:" Fazer pedido",
+    },
+
+    {
+        id: 24,
+        title: "Pizza Portuguesa",
+        categoria: "pizza",
+        preco: 40,
+        img: "img/pizza-portuguesa.jpeg",
+        descricao: "tamanho: Médio</br>ovo</br>presunto</br>milho</br>porção para 4 pessoas",
+        btn:" Fazer pedido",
+    },
+
+    {
+        id: 25,
+        title: "Hambúrgur Frango",
+        categoria: "hamburguer",
+        preco: 12,
+        img: "img/hamburguer-frango.jpg",
+        descricao: "pão brioche</br>ovo</br>presunto</br>alface</br>queijo</br>molho da casa",
+        btn:" Fazer pedido",
+    },
+
+    {
+        id: 26,
+        title: "Hambúrgur",
+        categoria: "hamburguer",
+        preco: 22,
+        img: "img/hamburguer-combo.jpeg",
+        descricao: "1 hambúrguer</br>Fanta</br>batata frita</br>molho especial",
+        btn:" Fazer pedido",
+    },
+
+    {
+        id: 27,
+        title: "Hambúrgur",
+        categoria: "hamburguer",
+        preco: 17,
+        img: "img/hamburguer-g.jpeg",
+        descricao: "Hambúrguer gourmet</br>400g</br>calabresa</br>molho da cassa</br>queijo mussarela</br>ovo",
+        btn:" Fazer pedido",
+    },
+
+
+    
+
+];
+
+const sectionCenter = document.querySelector(".section-center");
+
+const filterBtns = document.querySelectorAll('.filter-btn')
+
+
+window.addEventListener('DOMContentLoaded', function () {
+    displayMenuItems(menu);
+});
+
+
+
+filterBtns.forEach(function (btn) {
+    btn.addEventListener('click', function (e) {
+        const categoria = e.currentTarget.dataset.id
+        const menuCategoria = menu.filter(function (menuItems) {
+            //console.log(menuItems.categoria)
+            if (menuItems.categoria === categoria) {
+
+                return menuItems
+            }
+        });
+        //console.log(menuCategoria);
+        if(categoria === 'todos'){
+            displayMenuItems(menu);
+        }
+        else{
+           displayMenuItems(menuCategoria) 
+        }
+    });
+});
+
+function displayMenuItems(menuItems) {
+    let displayMenu = menuItems.map(function (item) {
+        // console.log(item);
+        return `<article class="menu-item">
+        <img src=${item.img}
+            alt=${item.title}>
+        <div class="item-info">
+            <header>
+                <h4>${item.title}</h4>
+                <h4 class="preco">$${item.preco}</h4>
+            </header>
+            <p class="item-text">
+                ${item.descricao}
+            </p>
+            <button class="btn btn-outline-success"><a href="https://wa.me/5589981034501" target="_blank">${item.btn}</a></button>
+        </div>
+    </article>`;
+    });
+    displayMenu = displayMenu.join("");
+
+    sectionCenter.innerHTML = displayMenu
+}
